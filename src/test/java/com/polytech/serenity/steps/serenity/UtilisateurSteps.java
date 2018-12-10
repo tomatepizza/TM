12f0db4d149aa6065fb74f7675888a2ee2b204b8
tomatepizza <45769770+tomatepizza@users.noreply.github.com>
2018-12-10 21:32:56 +0100

    Update UtilisateurSteps.java

--- src/test/java/com/polytech/serenity/steps/serenity/UtilisateurSteps.java
+++ src/test/java/com/polytech/serenity/steps/serenity/UtilisateurSteps.java
@@ -4,7 +4,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
-import com.polytech.serenity.pages.DictionnairePage;
 import com.polytech.serenity.pages.ConnexionPage;
 import com.polytech.serenity.pages.PokedexPage;
 import com.polytech.serenity.pages.VisionnerPage;
@@ -17,37 +16,9 @@ import net.thucydides.core.annotations.Step;
 
 public class UtilisateurSteps {
 
-    DictionnairePage dictionnaire;
     ConnexionPage connexion;
     PokedexPage pokedex;
     VisionnerPage visionner;
-
-    //Recherche
-    @Step
-    public void saisit_mot(String motCles) {
-        dictionnaire.entrer_mots_cles(motCles);
-    }
-
-    @Step
-    public void lance_la_recherche() {
-        dictionnaire.rechercher_mots();
-    }
-
-    @Step
-    public void doit_voir_la_definition(String definition) {
-        assertThat(dictionnaire.rechuperer_definitions(), hasItem(containsString(definition)));
-    }
-
-    @Step
-    public void est_sur_la_homepage() {
-        dictionnaire.open();
-    }
-
-    @Step
-    public void recherche(String motCle) {
-        saisit_mot(motCle);
-        lance_la_recherche();
-    }
     
     //Connexion
     @Step
